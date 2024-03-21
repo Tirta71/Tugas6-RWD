b8d0bacb4adff221e45ce05e08344ecc7e7697ff
tirta71 <tirta4132@gmail.com>
2024-03-22 01:29:49 +0700

    fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,25 +5,22 @@ document.addEventListener("DOMContentLoaded", function () {
   let totalGaji = 0;
   let pegawaiData = [];
 
-  // Mengambil data dari localStorage jika ada
   if (localStorage.getItem("pegawaiData")) {
     pegawaiData = JSON.parse(localStorage.getItem("pegawaiData"));
-    // Menambahkan data pegawai ke dalam tabel
+
     pegawaiData.forEach(function (data) {
       table.row.add(data).draw();
-      // Mengupdate total gaji dengan data yang sudah disimpan
+
       totalGaji += parseFloat(
         data[7].replace(/[^\d.,]/g, "").replace(",", ".")
       );
     });
   }
 
-  // Mengambil totalGaji dari localStorage jika ada
   if (localStorage.getItem("totalGaji")) {
     totalGaji = parseFloat(localStorage.getItem("totalGaji"));
   }
 
-  // Menampilkan total gaji yang disimpan di localStorage
   const totalGajiElement = document.getElementById("total-gaji-amount");
   totalGajiElement.textContent = formatCurrency(totalGaji);
 
@@ -52,16 +49,12 @@ document.addEventListener("DOMContentLoaded", function () {
       `<button class="hapus-pegawai btn btn-danger" data-nama="${nama}">Hapus</button>`,
     ];
 
-    // Menambahkan baris ke dalam tabel
     table.row.add(newRow).draw();
 
-    // Menambahkan data pegawai ke dalam array
     pegawaiData.push(newRow);
 
-    // Menyimpan data pegawai ke dalam localStorage
     localStorage.setItem("pegawaiData", JSON.stringify(pegawaiData));
 
-    // Mengupdate total gaji
     totalGaji += total;
     updateTotalGaji();
 
@@ -85,18 +78,14 @@ document.addEventListener("DOMContentLoaded", function () {
         cleanedTotal.replace(/\./g, "").replace(",", ".")
       );
 
-      // Mengupdate total gaji
       totalGaji -= totalRow;
       updateTotalGaji();
 
-      // Menghapus baris dari tabel
       table.row(row).remove().draw();
 
-      // Menghapus data pegawai dari array
       const rowIndex = table.row(row).index();
       pegawaiData.splice(rowIndex, 1);
 
-      // Menyimpan data pegawai ke dalam localStorage setelah data dihapus
       localStorage.setItem("pegawaiData", JSON.stringify(pegawaiData));
 
       Swal.fire({
@@ -110,7 +99,7 @@ document.addEventListener("DOMContentLoaded", function () {
   function updateTotalGaji() {
     const totalGajiElement = document.getElementById("total-gaji-amount");
     totalGajiElement.textContent = formatCurrency(totalGaji);
-    // Simpan total gaji ke localStorage setiap kali diperbarui
+
     localStorage.setItem("totalGaji", totalGaji);
   }
 
